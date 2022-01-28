07335d2df9ec73967e17f997c9bf79c9d586338c
pickabo <nhossain@ualberta.ca>
2022-01-28 01:32:18 -0700

    made it much more modular I guess

--- server.py
+++ server.py
@@ -33,14 +33,32 @@ from os import path
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
+        """
+            Function which handles http requests from clients
+            and returns the required requested information
+        """
         self.user_input = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.user_input)
+
+        # some previous code
+        # print ("Got a request of: %s\n" % self.user_input)
 
         # parse the user input and store as variables after parsing
-        self.data = self.parse_data()
-        file_name = self.data['filename']
-        http_protocol = self.data['http-ver']
-        http_method = self.data['http-request']
+        inp = self.user_input.decode('utf-8').split('\r\n')[0].split(" ")
+ 
+        # parts of client input
+        self.http_method = inp[0]
+        self.file_name = inp[1]
+        self.file_path = path.abspath("www") + inp[1]
+        self.http_protocol = inp[2]
+
+        # Dictionary containing all required status codes and their responses
+        self.http_responses = {
+            200: 'HTTP/1.1 200 OK\r\nContent-Type: {}\r\n\r\n{}\r\n',
+            301: 'HTTP/1.1 301 Moved Permanently\r\nLocation: {}\r\n\r\nConnection Closed\r\n\r\n',
+            404: 'HTTP/1.1 404 Not Found\r\n\r\nConnection Closed\r\n',
+            405: 'HTTP/1.1 405 Method Not Allowed\r\n\r\nConnection Closed\r\n',
+            505: 'HTTP/1.1 505 HTTP Version Not Support\r\n\r\nConnection Closed\r\n'
+        }
 
         # variable for storing http responses
         self.http_response = None
@@ -48,83 +66,55 @@ class MyWebServer(socketserver.BaseRequestHandler):
         # variable storing the supporting file types
         self.file_type = self.getFileType()
 
-        # preliminary tests for required http protocol and method
-        if http_protocol != "HTTP/1.1":
-            self.http_response = self.responses(505)
+        # preliminary tests for required http protocols and method
+        if self.http_protocol != "HTTP/1.1":
+            self.http_response = self.http_responses[505]
 
-        if http_method != "GET":
-            self.http_response = self.responses(405)
+        if self.http_method != "GET":
+            self.http_response = self.http_responses[405]
 
-        # if the data checks the input specs return as requested
+        # if the data checks the input specs return http response as requested
         corrected_path = self.check_path()
-        if corrected_path and self.http_response == None:        
-            print("The path given is", corrected_path)
-            
+        if corrected_path and self.http_response == None:                    
             if path.isfile(corrected_path):
                 if "www" in path.abspath(corrected_path):
                     file = open(corrected_path, 'r')
                     file_content = file.read()
                     file.close()
-                    self.http_response = self.responses(200).format(self.file_type , file_content)
+                    self.http_response = self.http_responses[200].format(self.file_type , file_content)
 
             else:
                 if not corrected_path[-1] == "/":
-                    self.http_response = self.responses(301).format(file_name + '/')
+                    self.http_response = self.http_responses[301].format(self.file_name + '/')
                     
                 elif path.isfile(corrected_path + "index.html"):
                     file = open(corrected_path + "index.html" , 'r')
                     file_content = file.read()
                     file.close()
-                    self.http_response = self.responses(200).format("text/html" , file_content)
+                    self.http_response = self.http_responses[200].format("text/html" , file_content)
 
- 
+        if self.http_response == None:
+            self.http_response = self.http_responses[404] 
 
         self.request.sendall(self.http_response.encode('utf-8'))
-
-
-    def parse_data(self):
-        # dividing up the input
-        inp = self.user_input.decode('utf-8').split('\r\n')[0].split(" ")
-        data = {
-            'http-request': inp[0],
-            'path': path.abspath("www") + inp[1],
-            'filename': inp[1],
-            'http-ver': inp[2]
-        }
-        return data
 
 
     def check_path(self):
         """
             function for checking if the data constitutes of the correct 
-             file path and returns True or False depending on the check
+            file path and returns True or False depending on the check
         """
-        file_path = self.data['path']
-        if path.exists(file_path):
-            return file_path
+        if path.exists(self.file_path):
+            return self.file_path
         else:
             return False
 
-    def getStatusResponse(self):
-        """
-            function for returning the http status response codes with
-            their respective messages
-        """
-        self.responses = {
-            200: 'HTTP/1.1 200 OK\r\nContent-Type: {}\r\n\r\n{}\r\n',
-            301: 'HTTP/1.1 301 Moved Permanently\r\nLocation: {}\r\n\r\nConnection Closed\r\n\r\n',
-            404: 'HTTP/1.1 404 Not Found\r\n\r\nConnection Closed\r\n',
-            405: 'HTTP/1.1 405 Method Not Allowed\r\n\r\nConnection Closed\r\n',
-            505: 'HTTP/1.1 505 HTTP Version Not Support\r\n\r\nThe specific HTTP version is not supported\r\n'
-        }
-
-
     def getFileType(self):
         """
-        function for getting the different filetypes
-        supported and unsupported
+            function for getting the different filetypes
+            supported and unsupported
         """
-        filename = self.data['path'].split()[-1]
+        filename = self.file_path.split()[-1]
         splitted = filename.split(".")
         filename_extension = ''
         if len(splitted) != 1:
